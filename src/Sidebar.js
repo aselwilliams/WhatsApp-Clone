630b2bc4f98afa2b042508c1e648dfe082c56959
aselwilliams <aselwilliams22@gmail.com>
2022-09-01 10:31:22 -0500

    sidebar header styled

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import './Sidebar.css'
+import { Avatar,IconButton } from '@mui/material';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Sidebar() {
   return (
-    <h1>Sidebar</h1>
+    <div className='sidebar'>
+        <div className="sidebar__header">
+            <Avatar />
+            <div className="sidebar__headerRight">
+            <IconButton>
+                <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+                <WhatsAppIcon />
+            </IconButton>
+            <IconButton>
+                <MoreVertIcon />
+            </IconButton>
+            </div>
+        </div>
+        <div className="sidebar__search">
+
+        </div>
+        <div className="sidebar__chats">
+
+        </div>
+
+    </div>
   )
 }
 
