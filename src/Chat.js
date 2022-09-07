1d37a5a5bec0f75308159c97c543fbfc3d8362f1
aselwilliams <aselwilliams22@gmail.com>
2022-09-06 23:16:59 -0500

    added realtime listeners to chat rooms

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,26 @@
 import { AttachFile, InsertEmoticon, SearchOutlined} from '@mui/icons-material';
 import MoreVert from '@mui/icons-material/MoreVert';
 import MicIcon from '@mui/icons-material/Mic';
-import { Avatar,IconButton } from '@mui/material'
+import { Avatar,IconButton } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
-import './Chat.css'
+import './Chat.css';
+import db from './firebase';
+import {collection, doc, onSnapshot} from 'firebase/firestore';
 
 function Chat() {
     const [seed, setSeed] =useState('');
     const [input, setInput] = useState('');
     const {roomId} = useParams();
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(()=> {
+        if(roomId) {
+            onSnapshot(doc(db, 'Rooms', roomId), (doc)=> (
+                setRoomName(doc.data().name)
+            ))
+        }
+    }, [roomId])
 
     useEffect(()=> {
         setSeed(Math.floor(Math.random * 5000))
@@ -26,7 +37,7 @@ function Chat() {
         <div className="chat__header">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="chat__headerInfo">
-                <h3>Room name</h3>
+                <h3>{roomName}</h3>
                 <p>Last seen at ...</p>
             </div>
             <div className="chat__headerRight">
